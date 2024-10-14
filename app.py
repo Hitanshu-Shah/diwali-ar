101460d101c41e0d10c510d693a80861742d2bfe
Hitanshu-Shah <120137068+Hitanshu-Shah@users.noreply.github.com>
2024-10-14 13:20:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,15 +10,10 @@ st.write("""
          """)
 
 # Link to the hosted AR file
-html_file = open("ar_greeting.html", 'r', encoding='utf-8').read()
+html_file = open("DiwaliAR.html", 'r', encoding='utf-8').read()
 
 # Embed the HTML file inside the Streamlit app
 st.components.v1.html(html_file, height=600, scrolling=True)
-
-# Optionally, if you want to show an image of the QR code, include that here
-st.write("Use your camera to scan this QR code:")
-qr_code_image = "qr_code.png"  # If you generated a QR code image, place it in the folder
-st.image(qr_code_image, caption='Scan to view the AR greeting!')
 
 # Provide a clickable link to the AR experience if needed
 st.markdown(f"[View AR Experience](./ar_greeting.html)")
